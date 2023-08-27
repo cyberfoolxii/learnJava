a8b529b08a18368c3d72a4b535f3c024926aa1e6
Cuong Nguyen <22028327@vnu.edu.vn>
2023-08-27 22:38:44 +0700

    Java variables test

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main{
 	public static void main(String[] args){
 	    // this is my first java application!
-		System.out.println("Hello World");
+	    int a, b, c;
+	    a = b = c = 69;
+	    String text = "value of a + b + c : ";
+		System.out.print(text);
+		System.out.println(a + b + c);
 	}
 }
