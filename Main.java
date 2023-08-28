8a89f4abad617ff3b91b04edc094453b0459a05f
Cuong Nguyen <22028327@vnu.edu.vn>
2023-08-28 17:08:36 +0700

    Widening Casting and Narrowing Casting in Java

--- Main.java
+++ Main.java
@@ -4,7 +4,12 @@ public class Main{
 	    int a, b, c;
 	    a = b = c = 69;
 	    String text = "value of a + b + c : ";
+	    long longVariable = 1000000L;
+	    double doubleVariable = longVariable;
+	    long longVariable1 = (long) doubleVariable;
 		System.out.print(text);
 		System.out.println(a + b + c);
+		System.out.println("value of doubleVariable : " + doubleVariable);
+		System.out.println("value of longVariable1 : " + longVariable1);
 	}
 }
